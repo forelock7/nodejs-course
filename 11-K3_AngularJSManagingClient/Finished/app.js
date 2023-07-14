6cef9fe374589d9a8dfdeb6410739177d27df60b
Volodymyr Chubenko <volodymyr.chubenko@sisense.com>
2023-07-14 18:36:11 +0300

    S11 - Front-Back End

--- 11-K3_AngularJSManagingClient/Finished/app.js
+++ 11-K3_AngularJSManagingClient/Finished/app.js
@@ -3,13 +3,23 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
+var people = [
+  {
+    name: 'John Doe',
+  },
+  {
+    name: 'Jane Doe',
+  },
+  {
+    name: 'Jim Doe',
+  },
+];
+
 app.set('view engine', 'ejs');
 app.use('/assets', express.static(__dirname + '/public'));
 
-app.get('/', function(req, res) {
-	
-	res.render('index');
-	
+app.get('/', function (req, res) {
+  res.render('index', { serverPeople: people });
 });
 
 app.listen(port);
